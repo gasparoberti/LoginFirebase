20644954e04b11264b344e09ae936d0821dca9fb
Gaspar <obertigaspar@gmail.com>
2020-07-22 16:23:45 -0300

    se separó en dos actividades y si te logueas va a otra actividad con el boton para cerrar sesion. si cerras sesion vuelve a la actividad de login. ERROR: si estas en el login y presionas atras queda en un bucle infinito y no sale de la app

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -3,74 +3,29 @@ package com.example.firebase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth mfirebaseAuth;
-    FirebaseAuth.AuthStateListener authStateListener;
-
-    public static final int REQUEST_CODE = 123;
-
-    List<AuthUI.IdpConfig> provider = Arrays.asList(
-            new AuthUI.IdpConfig.GoogleBuilder().build(),
-            new AuthUI.IdpConfig.FacebookBuilder().build(),
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-            new AuthUI.IdpConfig.GitHubBuilder().build()
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mfirebaseAuth = FirebaseAuth.getInstance();
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Toast.makeText(MainActivity.this, "Sesion de " + user.getEmail(), Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    startActivityForResult(AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(provider)
-                            .setIsSmartLockEnabled(false)
-                            .build(), REQUEST_CODE
-                    );
-                }
-            }
-        };
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mfirebaseAuth.addAuthStateListener(authStateListener);  //queda validada la sesion
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mfirebaseAuth.removeAuthStateListener(authStateListener);   //olvida la sesion
     }
 
     public void cerrarSesion(View view) {
         AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(MainActivity.this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "Sesión cerrada", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
             }
         });
     }
