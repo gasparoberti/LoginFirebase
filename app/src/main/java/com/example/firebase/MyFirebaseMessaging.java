4b7baff834a39b65360d5ef731b38a0e90c7fbe9
Gaspar <obertigaspar@gmail.com>
2020-07-25 02:29:57 -0300

    desde la consola de firebase llegan bien las notificaciones en segundo plano. las en primer plano no se por que me escriben el hola mundo. tengo que revisar mas el codigo. falta probar  enviar con postman usando el token y falta desde un servidor

---
+++ app/src/main/java/com/example/firebase/MyFirebaseMessaging.java
@@ -0,0 +1,54 @@
+package com.example.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+
+public class MyFirebaseMessaging extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.d(TAG, "onNewToken: " + token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "Mensaje recibido");
+        Map<String, String> data = remoteMessage.getData();
+        if (data.size() > 0) {
+            Log.d(TAG, "data: " + data);
+            String title = data.get("titulo");
+            String msg = data.get("mensaje");
+            sendNotification(title, msg);
+
+        } else{
+            RemoteMessage.Notification notification = remoteMessage.getNotification();
+            String title = notification.getTitle();
+            String msg = notification.getBody();
+
+            sendNotification(title, msg);
+        }
+
+    }
+
+    private void sendNotification(String title, String msg) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, MyNotification.NOTIFICATION_ID, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        MyNotification notification = new MyNotification(this, MyNotification.CHANNEL_ID_NOTIFICATIONS);
+        notification.build(R.drawable.ic_launcher_foreground, title, msg, pendingIntent);
+        notification.addChannel("Notificaciones", NotificationManager.IMPORTANCE_DEFAULT);
+        notification.createChannelGroup(MyNotification.CHANNEL_GROUP_GENERAL, R.string.notification_channel_group_general);
+        notification.show(MyNotification.NOTIFICATION_ID);
+    }
+}
